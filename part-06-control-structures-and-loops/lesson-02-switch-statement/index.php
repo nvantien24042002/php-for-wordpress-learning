7adba834439e3f998bfae2d711119533249e78c2
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-06 18:00:32 +0700

    learn: practice PHP switch statements

---
+++ part-06-control-structures-and-loops/lesson-02-switch-statement/index.php
@@ -0,0 +1,26 @@
+<?php
+$day = 2;
+switch($day){
+    case 2:
+        echo "Hôm nay là Thứ Hai";
+        break;
+    case 3:
+        echo "Hôm nay là Thứ Ba";
+        break;
+    case 4:
+        echo "Hôm nay là Thứ Tư";
+        break;
+    case 5:
+        echo "Hôm nay là Thứ Năm";
+        break;
+    case 6:
+        echo "Hôm nay là Thứ Sáu";
+        break;
+    case 7:
+        echo "Hôm nay là Thứ Bảy";
+        break;
+    default:
+        echo "Hôm nay là Chủ Nhật";
+        break;
+}
+?>
