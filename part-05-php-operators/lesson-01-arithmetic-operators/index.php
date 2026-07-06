1c630d0f84c6f88b06c7a1a7c8b88c8918fa9ea5
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-06 07:25:46 +0700

    learn: practice PHP arithmetic operators

---
+++ part-05-php-operators/lesson-01-arithmetic-operators/index.php
@@ -0,0 +1,15 @@
+<?php
+$a = 10;
+$b = 5;
+$total = $a + $b;
+echo $a + $b;
+echo "<br>";
+echo $a - $b;
+echo "<br>";
+echo $a * $b;
+echo "<br>";
+echo $a / $b;
+echo "<br>";
+
+echo "{$a} + {$b} = {$total}";
+?>
