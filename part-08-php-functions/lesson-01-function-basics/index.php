d7caf11126985f64ce6af3421ccedc719ae7aabe
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-09 13:45:38 +0700

    feat: findMax()

--- part-08-php-functions/lesson-01-function-basics/index.php
+++ part-08-php-functions/lesson-01-function-basics/index.php
@@ -13,4 +13,18 @@ $n = 3;
 echo ($n % 2 == 0) ? "$n là số chẵn" : "$n là số lẻ";
 echo "<br>";
 echo calculateRectangleArea(2,5);
+echo "<br>";
+// Bài tập 3 : Tìm số lớn nhất trong mảng
+function findMax($a){
+    $max = $a[0];
+    foreach($a as $item){
+        if($item > $max){
+            $max = $item;
+        }
+    }
+    return $max;
+}
+// Tests
+$numbers = [3, 7, 2, 9, 5, 1];
+echo "Số lớn nhất trong mảng là ".findMax($numbers);
 ?>
