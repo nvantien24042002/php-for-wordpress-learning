8266c93a40d37823d5efd954dc297904d11a3dc1
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-10 04:28:09 +0700

    learn: practice advanced PHP function parameters_2

--- part-08-php-functions/lesson-03-function-parameters/index.php
+++ part-08-php-functions/lesson-03-function-parameters/index.php
@@ -2,18 +2,36 @@
 function sum($a,$b){
     return $a + $b;
 }
-function sum_multi_number() {
-    // echo func_num_args();
-    // func_get_args() trả về một mảng chứa tất cả các đối số
-    // $a = func_get_arg(0);
-    // $b = func_get_arg(1);
-    // echo "a = {$a} <br> b = {$b}";
-    $list_args = func_get_args();
-    $t = 0;
-    foreach ($list_args as $value) {
-        $t += $value;
+// function sum_multi_number() {
+//     // echo func_num_args();
+//     // func_get_args() trả về một mảng chứa tất cả các đối số
+//     // $a = func_get_arg(0);
+//     // $b = func_get_arg(1);
+//     // echo "a = {$a} <br> b = {$b}";
+//     $list_args = func_get_args();
+//     $t = 0;
+//     foreach ($list_args as $value) {
+//         $t += $value;
+//     }
+//     echo $t;
+// }
+function sum_multi_number($list_number = array()){
+    if(is_array($list_number)){
+        $t = 0;
+        foreach($list_number as $item){
+            $t+=$item;
+        }
+        echo $t;
     }
-    echo $t;
 }
-sum_multi_number(2, 5);
+$list_number = array(1,4,5);
+sum_multi_number($list_number);
+echo "<br>";
+function create_input_text($name,$value,$option = array()){
+    $id = isset($option['id']) ? $option['id'] : '';
+    $class = isset($option['class']) ? $option['class'] : '';
+    $html = "<input type='text' name='{$name}' value='{$value}' id='{$id}' class=''>";
+    return $html;
+}
+echo create_input_text('username', 'Nguyen Van A');
 ?>
